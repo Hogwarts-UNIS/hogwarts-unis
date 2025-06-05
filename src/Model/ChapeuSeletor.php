7bc32b95479a3e63886e1e9e9e908f13a255ed8d
DiogoSouza <diogohg24@gmail.com>
2025-06-05 19:54:19 -0300

    integraçao do namespace

--- src/Model/ChapeuSeletor.php
+++ src/Model/ChapeuSeletor.php
@@ -1,4 +1,12 @@
 <?php
 
+namespace App\Model\Grifinoria;
+namespace app\model\Sonserina;
+namespace app\model\Corvinal;
+namespace app\model\LufaLUfa;
+
     class ChapeuSeletor 
+    {
+        private array $casas = 
+    }
 
