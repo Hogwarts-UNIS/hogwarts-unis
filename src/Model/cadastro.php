87645fad96d01d84e9dc39127789f1713f80753c
jussara6 <jussara3856@gmail.com>
2025-06-12 19:59:17 -0300

    cadastro=funções

--- src/Model/cadastro.php
+++ src/Model/cadastro.php
@@ -2,22 +2,40 @@
 
 namespace App\Model;
 use App\Model\Bruxo;
-class Professor extends Bruxo {
-    
-    protected string $disciplina;
-    protected ?\DateTime $dataContratacao = null;
+class Cadastro {
 
-    public function __construct(string $nome, int $idade, string $email, string $disciplina) {
-        parent::__construct($nome, $idade, $email, 'professor');
+    private string $professor;
+    private string $disciplina;
+    private string $funcionario;
+    private string $turma;
+    private string $horarios;
+
+    public function __construct(string $professor, string $disciplina, string $funcionario, string $turma, string $horarios) {
         $this->disciplina = $disciplina;
-        $this->dataContratacao = new \DateTime();
+        $this->professor = $professor;
+        $this->funcionario = $funcionario;
+        $this->turma = $turma;
+        $this->horarios = $horarios;
     }
-    
-    public function getDisciplina(): string {
-        return $this->disciplina;
+    public function cadastrarProfessor(Bruxo $bruxo): void {
+        $this->professor = $bruxo->getNome();
+        echo "Professor {$this->professor} cadastrado com sucesso na disciplina {$this->disciplina}.\n";
+    }
+    public function cadastrarFuncionario(Bruxo $bruxo): void {
+        $this->funcionario = $bruxo->getNome();
+        echo "Funcionário {$this->funcionario} cadastrado com sucesso.\n";
+    }
+    public function HorarioDasTurmas(string $turma, string $horarios):void{
+        $this->turma=$turma.
+        $this->horarios=$horarios;
+        echo "A turma {$this->turma} possui os seguintes horários: {$this->horarios}.\n";
+    }
+    public function DisciplinaMinistrada (string $disciplina, string $professor, string $turma):void{
+        $this->disciplina=$disciplina;
+        $this->professor=$professor;
+        $this->turma=$turma;
+        echo "O professor {$this->professor} irá ministrar {$this->disciplina} para a turma {$this->turma}.\n";
+    }
     }
 
-    public function getDataContratacao(): ?\DateTime {
-        return $this->dataContratacao;
-    }
-}
+
