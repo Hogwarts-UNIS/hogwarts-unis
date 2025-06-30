ac0b16757fd4491a3fab3fccfcd92d4f244f23b0
jussara6 <jussara3856@gmail.com>
2025-06-30 09:21:25 -0300

    professor e funcionario concluído

--- src/Model/funcionario.php
+++ src/Model/funcionario.php
@@ -1,4 +1,34 @@
 <?php
 class funcionario{
-    
-}
+    private $nome;
+    private $cargo;
+    private $setor;
+
+    public function __construct($nome, $cargo, $setot){
+        $this->nome=$nome;
+        $this->cargo=$cargo;
+        $this->setor=$settor;
+    }
+    public function getNome(){
+        return $this->nome;
+    }
+    public function getCargo(){
+        return $this->cargo;
+    }
+    public function getSetor(){
+        return $this->setor;
+    }
+    public function __toString(){
+        return sprintf("%s\n  Cargo:%s \n  Setor:%s",
+        $this->nome,
+        $this->cargo,
+        $this->setor);
+    }
+
+
+
+
+
+
+
+           }
--- src/Model/professor.php
+++ src/Model/professor.php
@@ -1,4 +1,71 @@
 <?php
 class professor{
-    
+    private $nome;
+    private $disciplinas;
+    private $turmas;
+    private $cronograma;
+  
+
+    public function __construct($nome){
+        $this->nome = $nome;
+        $this->disciplinas = [];
+        $this->turmas = [];
+        $this->cronograma = [];
+    }
+    public function abrirCronograma (){
+       $diasSemana = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"];
+        foreach ($diasSemana as $dia) {
+            $this->cronograma[$dia] = [];
+    }
 }
+public function getNome(){
+    return $this->nome;
+}
+public function getDisciplinas(){
+    return $this->disciplinas;
+}
+public function getTurmas(){
+    return $this->turmas;
+}
+public function getCronograma(){
+    return $this->cronograma;
+}
+    public function AddDisciplina ($disciplina){
+        if (!in_array($disciplina, $this->disciplinas)) {
+            $this->disciplinas[] = $disciplina;
+        }   
+    }
+    public function AddTurma ($turma){
+        if (!in_array($turma, $this->turmas)) {
+            $this->turmas[] = $turma;
+        }   
+    }
+    public function AddHorario ($dia, $horario){
+        if (array_key_exists($dia,$this->cronograma)){
+            $this->cronograma[$dia][] = $horario;
+        }
+    }
+     public function exibirCronograma() {
+        echo "\n📅 Cronograma do Professor " . $this->nome . ":\n";
+        echo "══════════════════════════════════════\n";
+        foreach ($this->cronograma as $dia => $horarios) {
+            printf("%-10s: ", $dia);
+            if (empty($horarios)) {
+                echo "Livre\n";
+            } else {
+                echo implode(", ", $horarios) . "\n";
+            }
+        }
+    }
+    public function __toString() {
+     $disciplinasStr = implode(", ", $this->disciplinas);
+        $turmasStr = implode(", ", $this->turmas);
+        return sprintf("%s\n   Disciplinas: %s\n   Turmas: %s", 
+            $this->nome, $disciplinasStr, $turmasStr);
+    }
+}
+
+
+
+
+
